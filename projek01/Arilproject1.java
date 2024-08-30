5a2521cc67572fbe58769842bc1713daf9770a72
ariel <arielnumara@gmail>
2024-08-30 13:28:16 +0700

    tambah projek1

---
+++ projek01/Arilproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arilproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class Arilproject1{
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Ariel Numara Ravi Y");
+        System.out.println("NPM  : 2330506052 ");
+        System.out.println("Alamat: Magelang");
+        System.out.println("No hp : 089516123021");
+    }
+}
